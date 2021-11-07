d05c7af71b98fd5ab4a0dfde9150416bb45851a0
Bruno Oliveira <bruno.mktd@gmail.com>
2021-11-07 10:43:04 -0300

    Adicionando novos nomes na lista

--- src/app/lista-de-tarefas/lista-de-tarefas.component.ts
+++ src/app/lista-de-tarefas/lista-de-tarefas.component.ts
@@ -13,7 +13,9 @@ export class ListaDeTarefasComponent implements OnInit {
   familia: Array<string> = [
     "Matheus",
     "Jucelino",
-    "Claudia"
+    "Claudia",
+    "Lucas",
+    "Leticia"
 
   ];
 
